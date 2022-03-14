5478c4d87e0d772c8f9c791c664a0b0724a1a30b
GMECHMEC <ghaith.mechmech.externe@odigo.com>
2022-03-14 13:02:32 +0100

    Initial commit

---
+++ src/main/java/com/libon/test/tondeuseGazon/TondeuseGazonApplication.java
@@ -0,0 +1,13 @@
+package com.libon.test.tondeuseGazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TondeuseGazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TondeuseGazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libon/test/tondeuseGazon/TondeuseGazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libon.test.tondeuseGazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TondeuseGazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
