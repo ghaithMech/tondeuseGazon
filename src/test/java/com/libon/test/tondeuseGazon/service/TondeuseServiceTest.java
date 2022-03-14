4def95278d64a54a0ae1de84b08349a0a8f2628a
GMECHMEC <ghaith.mechmech.externe@odigo.com>
2022-03-14 16:04:08 +0100

    service

--- src/test/java/com/libon/test/tondeuseGazon/service/TondeuseServiceTest.java
+++ src/test/java/com/libon/test/tondeuseGazon/service/TondeuseServiceTest.java
@@ -2,19 +2,14 @@ package com.libon.test.tondeuseGazon.service;
 
 import com.libon.test.tondeuseGazon.domain.*;
 import com.libon.test.tondeuseGazon.service.exception.InstructionException;
-import com.libon.test.tondeuseGazon.service.exception.OrientationException;
 import com.libon.test.tondeuseGazon.service.exception.PositionException;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@ExtendWith(MockitoExtension.class)
 public class TondeuseServiceTest {
 
     private static final String INSTRUCTIONS1 = "GAGAGAGAA";
@@ -24,20 +19,22 @@ public class TondeuseServiceTest {
     private final Dimension dimension = new Dimension(5, 5);
     private final Position position1 = new Position(1, 2, Orientation.N);
     private final Position position2 = new Position(3, 3, Orientation.E);
+    private final Position overPosition1 = new Position(1, 2, Orientation.N);
+    private final Position overPosition2 = new Position(3, 3, Orientation.E);
     private final Position fakePosition1 = new Position(1, 17, Orientation.N);
     private final Position fakePosition2 = new Position(-1, 3, Orientation.E);
+    private final Position fakePosition3 = new Position(1, 3, Orientation.E);
     private final Resultat resultat = new Resultat(List.of(new Position(1, 3, Orientation.N), new Position(5, 1, Orientation.E)));
     private final Resultat overResultat = new Resultat(List.of(new Position(5, 3, Orientation.N), new Position(5, 1, Orientation.E)));
-    @Mock
-    private TondeuseService service;
+    private final TondeuseService service = new TondeuseService();
 
     @Test
     void getPosition() {
-        assertThrows(InstructionException.class,()->service.getPosition(new Payload(dimension, List.of(new Mower(position1, FAKEINSTRUCTIONS), new Mower(position2, INSTRUCTIONS2)))));
-        assertThrows(PositionException.class,()->service.getPosition(new Payload(dimension, List.of(new Mower(fakePosition1, INSTRUCTIONS1), new Mower(position2, INSTRUCTIONS2)))));
-        assertThrows(PositionException.class,()->service.getPosition(new Payload(dimension, List.of(new Mower(fakePosition2, INSTRUCTIONS1), new Mower(position2, INSTRUCTIONS2)))));
+        assertThrows(InstructionException.class, () -> service.getPosition(new Payload(dimension, List.of(new Mower(fakePosition3, FAKEINSTRUCTIONS), new Mower(fakePosition3, INSTRUCTIONS2)))));
+        assertThrows(PositionException.class, () -> service.getPosition(new Payload(dimension, List.of(new Mower(fakePosition1, INSTRUCTIONS1), new Mower(fakePosition3, INSTRUCTIONS2)))));
+        assertThrows(PositionException.class, () -> service.getPosition(new Payload(dimension, List.of(new Mower(fakePosition2, INSTRUCTIONS1), new Mower(fakePosition3, INSTRUCTIONS2)))));
         assertEquals(resultat, service.getPosition(new Payload(dimension, List.of(new Mower(position1, INSTRUCTIONS1), new Mower(position2, INSTRUCTIONS2)))));
-        assertEquals(overResultat, service.getPosition(new Payload(dimension, List.of(new Mower(position1, OVERINSTRUCTIONS), new Mower(position2, INSTRUCTIONS2)))));
+        assertEquals(overResultat, service.getPosition(new Payload(dimension, List.of(new Mower(overPosition1, OVERINSTRUCTIONS), new Mower(overPosition2, INSTRUCTIONS2)))));
     }
 
 }
